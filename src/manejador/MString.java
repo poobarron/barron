f24b2f7eef3adcf18896183c070de142a6411be4
luis enrique <luisesh2@hotmail.com>
2017-02-19 02:32:01 -0600

    primer ejercicio

--- src/manejador/MString.java
+++ src/manejador/MString.java
@@ -1,5 +1,12 @@
 package manejador;
 
 public class MString {
-
+	public int numero_de_incidencias(String cadena, char caracter){
+		int repeticiones=0;
+		for(int  i=0;i<cadena.length();i++){
+			if (cadena.charAt(i)==caracter)
+				repeticiones++;
+		}
+		return repeticiones; 
+	}
 }
--- src/manejador/Principal.java
+++ src/manejador/Principal.java
@@ -5,8 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MString obj = new MString();
-		
-	
+		System.out.println("numero de repeticiones:" + obj.numero_de_incidencias("anita lava la tina", 'A'));
 	}
 
 }
